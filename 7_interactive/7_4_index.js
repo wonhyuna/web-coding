e55f2b160518ba2a3bbf3fa921c9ed60178f1626
wonhyuna <hyunawon417@gmail.com>
2023-08-16 14:31:23 +0900

    Web-Coding 인터랙티브 DOM 트리, textContext, outerHTML, innerHTML

--- 7_interactive/7_4_index.js
+++ 7_interactive/7_4_index.js
@@ -1,6 +1,7 @@
-// DOM 트리 여행하기
+// DOM 트리 프로퍼티
 const myTag = document.querySelector('#list-1');
 
+//본인
 console.log(myTag);
 
 // 형제 요소 노드
@@ -14,3 +15,17 @@ console.log(myTag.parentElement);
 console.log(myTag.children[1]);
 console.log(myTag.firstElementChild);
 console.log(myTag.lastElementChild);
+
+
+
+// textContext: HTML태그를 제외하고 text만 가지고 옴 (줄바꿈, 들여쓰기 모두 포함)
+console.log(myTag.textContent);
+// myTag.textContent = 'new text!';
+
+// outerHTML
+console.log(myTag.outerHTML);
+// myTag.outerHTML = '<ul id = "new-list"><li>Exotic</li></ul>';
+
+// innerHTML (줄바꿈, 들여쓰기 모두 포함)
+console.log(myTag.innerHTML);
+// myTag.innerHTML += '<li>Exotic</li>';
